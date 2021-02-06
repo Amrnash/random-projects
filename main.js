21b5233247ac84dfb641d4463320b56f9352393c
Muhammad Nashaat <mohamed.nashaat@mo4group.com>
2021-02-06 09:06:09 +0200

    careated the html and the style for the image slider

---
+++ main.js
@@ -0,0 +1,17 @@
+// select the slider-container and slider
+const slider = document.querySelector(".slider-container"),
+  slides = Array.from(document.querySelectorAll(".slider"));
+// the state
+let isDragging = false,
+  startPos = 0,
+  currentTranslate = 0,
+  prevTranslate = 0,
+  animationID = 0,
+  currentIndex = 0;
+// adding eventlisteneres on each slide
+slides.forEach((slide, index) => {
+  // remove the default behvior of dragstart event on img
+  const slideImage = slide.querySelector("img");
+  slideImage.addEventListener("dragstart", (e) => e.preventDefault());
+  // Mouse Events
+});
